142dc6f82ed4292349b45c49070aae3b9a84dc48
Kingsley Loo-Xu <kingsleyloolay@outlook.com>
2025-01-18 17:09:56 -0800

    added header

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './Header.css';
+// Supports weights 100-700
+import '@fontsource-variable/roboto-mono';
+
+function Header() {
+    return (
+        <div className="Header">
+            <h1>Neighbours In Need</h1>
+        </div>
+    );
+
+}
+
+export default Header;
